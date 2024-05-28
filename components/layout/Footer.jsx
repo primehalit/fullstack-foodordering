84f9172ec128bd8575d059f274d275ac43dc3d72
primehalit <halitmzrk04@gmail.com>
2024-05-28 17:55:33 +0300

    update package-lock

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -21,44 +21,44 @@ const Footer = () => {
 
   const getSocialMediaColor = (icon) => {
     switch (icon) {
-      case 'fa fa-facebook':
-        return 'bg-blue-700 text-white'; // Facebook için mavi renk
-      case 'fa fa-instagram':
-        return 'bg-pink-600 text-white'; // Instagram için pembe renk
-      case 'fa fa-twitter':
-        return 'bg-blue-400 text-white'; // Twitter için mavi renk
-      case 'fa fa-linkedin':
-        return 'bg-blue-500 text-white'; // LinkedIn için mavi renk
-      case 'fa fa-youtube':
-        return 'bg-red-600 text-white'; // YouTube için kırmızı renk
-      case 'fa fa-pinterest':
-        return 'bg-red-500 text-white'; // Pinterest için kırmızı renk
-      case 'fa fa-tiktok':
-        return 'bg-black text-white'; // TikTok için siyah renk
-      case 'fa fa-snapchat':
-        return 'bg-yellow-600 text-white'; // Snapchat için sarı renk
-      case 'fa fa-whatsapp':
-        return 'bg-green-600 text-white'; // WhatsApp için yeşil renk
-      case 'fa fa-telegram':
-        return 'bg-blue-200 text-white'; // Telegram için açık mavi renk
-      case 'fa fa-discord':
-        return 'bg-purple-700 text-white'; // Discord için mor renk
-      case 'fa fa-medium':
-        return 'bg-black text-white'; // Medium için siyah renk
-      case 'fa fa-reddit':
-        return 'bg-orange-500 text-white'; // Reddit için turuncu renk
-      case 'fa fa-tumblr':
-        return 'bg-blue-400 text-white'; // Tumblr için mavi renk
-      case 'fa fa-vimeo':
-        return 'bg-blue-600 text-white'; // Vimeo için mavi renk
-      case 'fa fa-skype':
-        return 'bg-blue-500 text-white'; // Skype için mavi renk
+      case "fa fa-facebook":
+        return "bg-blue-700 text-white"; // Facebook için mavi renk
+      case "fa fa-instagram":
+        return "bg-pink-600 text-white"; // Instagram için pembe renk
+      case "fa fa-twitter":
+        return "bg-blue-400 text-white"; // Twitter için mavi renk
+      case "fa fa-linkedin":
+        return "bg-blue-500 text-white"; // LinkedIn için mavi renk
+      case "fa fa-youtube":
+        return "bg-red-600 text-white"; // YouTube için kırmızı renk
+      case "fa fa-pinterest":
+        return "bg-red-500 text-white"; // Pinterest için kırmızı renk
+      case "fa fa-tiktok":
+        return "bg-black text-white"; // TikTok için siyah renk
+      case "fa fa-snapchat":
+        return "bg-yellow-600 text-white"; // Snapchat için sarı renk
+      case "fa fa-whatsapp":
+        return "bg-green-600 text-white"; // WhatsApp için yeşil renk
+      case "fa fa-telegram":
+        return "bg-blue-200 text-white"; // Telegram için açık mavi renk
+      case "fa fa-discord":
+        return "bg-purple-700 text-white"; // Discord için mor renk
+      case "fa fa-medium":
+        return "bg-black text-white"; // Medium için siyah renk
+      case "fa fa-reddit":
+        return "bg-orange-500 text-white"; // Reddit için turuncu renk
+      case "fa fa-tumblr":
+        return "bg-blue-400 text-white"; // Tumblr için mavi renk
+      case "fa fa-vimeo":
+        return "bg-blue-600 text-white"; // Vimeo için mavi renk
+      case "fa fa-skype":
+        return "bg-blue-500 text-white"; // Skype için mavi renk
       // Diğer sosyal medya ikonları için gerekli renkleri ekleyin
       default:
-        return 'bg-gray-600 text-white'; // Varsayılan olarak gri renk
+        return "bg-gray-600 text-white"; // Varsayılan olarak gri renk
     }
   };
-  
+
   return (
     <div className="bg-white text-black">
       <div className="container mx-auto pt-16 pb-6">
@@ -89,7 +89,9 @@ const Footer = () => {
               {footer?.socialMedia?.map((item) => (
                 <a
                   href={item?.link}
-                  className={`w-8 h-8 grid place-content-center rounded-full ${getSocialMediaColor(item.icon)}`}
+                  className={`w-8 h-8 grid place-content-center rounded-full ${getSocialMediaColor(
+                    item.icon
+                  )}`}
                   key={item._id}
                   target="_blank"
                   rel="noreferrer"
@@ -116,7 +118,7 @@ const Footer = () => {
           </div>
         </div>
         <p className="text-center mt-10">
-          © Halit MIZRAK Tarafından Geliştirildi...
+          © Halit MIZRAK Tarafından Geliştirildi.
         </p>
       </div>
     </div>
